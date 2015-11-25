bec646da1da8f4898aa86c2391686e165dae9b47
Erik Godard <godarderik@gmail.com>
2015-11-25 17:12:20 -0500

    added error handling

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import sys
 import datetime
 
-#todo - implement more error checking here
+#convert a deal from a string into a key value dictionary
 def parseQuery(line):
     searchParams = {}
     searchParams["dealFile"] = line[1]
     searchParams["hotel"] = line[2]
-    searchParams["startDate"] = line[3]
-    searchParams["duration"] = line[4]
+    try: 
+        searchParams["startDate"] = parseDate(line[3])
+    except:
+        print "Error: Invalid Start Date"
+        return {}
+    try: 
+        searchParams["duration"] = int(line[4])
+    except:
+        print "Error: Invalid Duration"
+        return {}
     return searchParams
 
+#convert a search query into a key value dictionary
 def parseDeal(line):
     splitLine = line.split(',')
     dealParams = {}
@@ -22,38 +31,54 @@ def parseDeal(line):
     dealParams["endDate"] = splitLine[6]
     return dealParams
 
+#apply a given discount to a given initial cost
 def applyDiscount(discountType, initialCost, value):
     if discountType == "rebate" or discountType == "rebate_3plus":
         return initialCost - value
     elif discountType == "pct":
         return int(initialCost * (100 - abs(value))/100.0)
 
-
+#convert a date string into a datetime object
 def parseDate(date):
     splitDate = date.split("-")
     return datetime.date(int(splitDate[0]), int(splitDate[1]), int(splitDate[2]))
 
+#return True if date falls between startDate and endDate, and False otherwise
 def inDateRange(date, startDate, endDate):
     return date - startDate > datetime.timedelta(seconds = 0) and endDate - date > datetime.timedelta(seconds = 0)
 
 
 def findBestDeal(query):
     searchParams = parseQuery(query)
+    if searchParams == {}:
+        return 
+
     bestDeal = {}
     bestCost = 9999999999
 
-    f = open(searchParams["dealFile"])
+    try: 
+        f = open(searchParams["dealFile"])
+    except: 
+        return "Error: Unable to open given file"
     for line in f:
         dealParams = parseDeal(line)
-        if not (dealParams["hotel"] == searchParams["hotel"]): #if we are not looking at the correct hotel 
+        #test if we are looking at the correct hotel
+        if not (dealParams["hotel"] == searchParams["hotel"]): 
             continue
-        elif not inDateRange(parseDate(searchParams["startDate"]), parseDate(dealParams["startDate"]), parseDate(dealParams["endDate"])): #test date here
+        #test if the start of our reservation falls within the booking period
+        elif not inDateRange(searchParams["startDate"], parseDate(dealParams["startDate"]), parseDate(dealParams["endDate"])):
             continue
+        #test if the deal is a rebate_3plus and we are staying for fewew than three days
         elif dealParams["type"] == "rebate_3plus" and searchParams["duration"] < 3:
             continue
         else: 
+            #find the cose of the stay before the deal
             baseCost = int(searchParams["duration"]) * int(dealParams["price"])
+
+            #find the cost of the stay after the deal
             baseCost = applyDiscount(dealParams["type"], baseCost, int(dealParams["value"]))
+
+            #update the best deal
             if baseCost < bestCost:
                 bestCost = baseCost
                 bestDeal = dealParams
@@ -70,7 +95,9 @@ if __name__ == "__main__":
     elif len(sys.argv) > 5: 
         print "Error: Too many arguments supplied"
     else:
-        print findBestDeal(args)
+        res = findBestDeal(args)
+        if res:
+            print res
 
 
 
